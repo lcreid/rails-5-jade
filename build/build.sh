2d6b5a6667bc9aa09ea0b1e3b6cbbaa1f37aaaae
Larry Reid <lcreid@jadesystems.ca>
2016-06-09 16:32:41 -0700

    Commit to test on other platforms.

--- build/build.sh
+++ build/build.sh
@@ -3,8 +3,7 @@
 # This should be treated as notes, rather than a single script.
 # There are a number of places where it's convenient to
 
-sudo apt-get install openssh-server linux-headers-$(uname -r) build-essential dkms; \
-sudo apt-get update; sudo apt-get dist-upgrade
+sudo apt-get install openssh-server linux-headers-$(uname -r) build-essential dkms
 # Now: Set up the insecure key according to the instructions at:
 # https://www.vagrantup.com/docs/boxes/base.html, about half way down the page
 # Also, do the other instructions to allow vagrant to sudo without a password
@@ -23,7 +22,10 @@ sudo apt-get install -y nodejs
 # Install Postgres
 apt-get install postgresql-9.5 postgresql-server-dev-9.5
 sudo gem install pg
+# Need the following if you're going to build webkit for Capybara
+sudo apt-get install libqtwebkit-dev gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-x
 # Clean up
+sudo apt-get update; sudo apt-get dist-upgrade
 sudo apt-get autoremove
 # Checkpoint the VM before installing Rails because it's still beta
-sudo gem install rails -v 5.0.0.beta3 --pre
+sudo gem install rails -v 5.0.0.beta4 --pre
