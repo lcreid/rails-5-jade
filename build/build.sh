8631277838d01b83ba4f2d5cbd0e3b54a68c11b8
Larry Reid <lcreid@jadesystems.ca>
2016-07-09 08:59:52 -0700

    Add Graphviz for Rails ERD.
    Added the install of Graphviz.
    Also moved the provisioning to the build.sh file, where it's easier
    to comment and see.

--- build/build.sh
+++ build/build.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
+sleep 30
 
-# This should be treated as notes, rather than a single script.
-# There are a number of places where it's convenient to
+# Set up insecure key (standard Vagrant practice)
+sudo mkdir ~vagrant/.ssh
+sudo chmod 700 ~vagrant/.ssh
+sudo wget --no-check-certificate 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' -O ~vagrant/.ssh/authorized_keys
+sudo chmod 600 ~vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant ~vagrant/.ssh
 
-sudo apt-get install openssh-server linux-headers-$(uname -r) build-essential dkms
-# Now: Set up the insecure key according to the instructions at:
-# https://www.vagrantup.com/docs/boxes/base.html, about half way down the page
-# Also, do the other instructions to allow vagrant to sudo without a password
-# Restart the guest
-sudo mount /dev/cdrom /media/cdrom
-sudo /media/cdrom/VBoxLinuxAdditions.run
-# Restart the guest
-# Checkpoint the VM now that the base OS and tools are ready.
-sudo apt-get install ruby sqlite3 libsqlite3-dev ruby-dev
+sudo apt-get update -y -qq
+sudo apt-get install -y -q virtualbox-guest-utils virtualbox-guest-dkms
+sudo apt-get install -y -q linux-headers-$(uname -r) build-essential dkms
+sudo apt-get install -y -q ruby sqlite3 libsqlite3-dev ruby-dev
+
 sudo gem install jekyll
 # Nokogiri build dependencies (from http://www.nokogiri.org/tutorials/installing_nokogiri.html#ubuntu___debian)
-sudo apt-get install patch zlib1g-dev liblzma-dev
+sudo apt-get install -y -q patch zlib1g-dev liblzma-dev
 # Install Node (from https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions)
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y -q -y nodejs
 # Install Postgres
-apt-get install postgresql-9.5 postgresql-server-dev-9.5
+sudo apt-get install -y -q postgresql-9.5 postgresql-server-dev-9.5
 sudo gem install pg
 # Need the following if you're going to build webkit for Capybara
-sudo apt-get install libqtwebkit-dev gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-x
+sudo apt-get install -y -q libqtwebkit-dev gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-x xvfb
+# Install support for Rails ERD http://voormedia.github.io/rails-erd/install.html
+sudo apt-get -y -q install graphviz
 # Clean up
-sudo apt-get update; sudo apt-get dist-upgrade
-sudo apt-get autoremove
-# Checkpoint the VM before installing Rails because it's still beta
-sudo gem install rails -v 5.0.0.rc2 --pre
+sudo apt-get dist-upgrade -y -qq
+sudo apt-get autoremove -y -qq
+
+sudo gem install rails -v 5.0.0
