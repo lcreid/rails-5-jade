d8f68f73130fceee1e5dd563030c89816b2f60d5
Larry Reid <lcreid@jadesystems.ca>
2018-04-09 08:46:28 -0700

    Add build of Tiny TDS.

--- build/mssql.sh
+++ build/mssql.sh
@@ -5,6 +5,7 @@ sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubunt
 sudo apt-get -y -q update
 # Choose developer edition
 sudo apt-get install -y -q mssql-server
+# https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-configure-environment-variables
 ACCEPT_EULA=y MSSQL_PID=Developer MSSQL_LCID=1033 MSSQL_SA_PASSWORD="MSSQLadmin!" sudo -E /opt/mssql/bin/mssql-conf setup
 sudo systemctl restart mssql-server.service
 
@@ -15,3 +16,16 @@ sudo apt-get -y -q update
 ACCEPT_EULA=y DEBIAN_FRONTEND=noninteractive sudo -E apt-get install -y -q --no-install-recommends mssql-tools unixodbc-dev
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+
+# Tiny TDS
+# https://github.com/rails-sqlserver/tiny_tds#install
+apt-get install build-essential
+apt-get install libc6-dev
+
+wget http://www.freetds.org/files/stable/freetds-1.00.86.tar.gz
+tar -xzf freetds-1.00.86.tar.gz
+cd freetds-1.00.86
+./configure --prefix=/usr/local --with-tdsver=7.3
+make
+sudo make install
+cd -
